f15c7897ed6370c9a6cf1cf9fb40e924b4731475
André <andre.bonaf@gmail.com>
2021-10-08 02:06:44 -0300

    estilos css 2

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 class Pokemon extends React.Component {
     render() {
-        const { name, type, averageWeight, image } = this.props.pokemon;
+        const { name, type, averageWeight, image, id } = this.props.pokemon;
 
         return (
             <div className="pokemon">
                 <div>
+                    <p className="pkm-id">{`#${id}`}</p>
                     <p> {name} </p>
                     <p> {type} </p>
                     <p> {`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
